500519e5b8e33f91fb15a38930d16bec0e9972b5
sasza <sasza@onet.eu>
2019-10-26 21:49:37 +0200

    int param 1

---
+++ src/errors.js
@@ -0,0 +1,10 @@
+const NOT_A_NUMBER = {
+  id: 1,
+  text: 'Not a number',
+}
+
+const ERROR = {
+  NOT_A_NUMBER,
+}
+
+export default ERROR
---
+++ src/params/IntParam.js
@@ -0,0 +1,19 @@
+import ERROR from '../errors'
+
+const validate = (value) => {
+  const number = parseInt(value, 10)
+  if (Number.isNaN(number)) return { value, error: ERROR.NOT_A_NUMBER }
+  return null
+}
+
+class IntParam {
+  constructor(conditions) {
+    this.conditions = conditions;
+  }
+
+  validate(value) {
+    return validate(value, this.conditions)
+  }
+}
+
+export default IntParam
