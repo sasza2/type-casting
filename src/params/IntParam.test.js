42c9f9828d2a8e21cfc70c351d83a797b6d05492
sasza <sasza@onet.eu>
2019-10-30 21:43:54 +0100

    int cast test

--- src/params/IntParam.test.js
+++ src/params/IntParam.test.js
@@ -1,5 +1,5 @@
 import ERROR from '../errors'
-import { validate } from './IntParam'
+import IntParam, { validate } from './IntParam'
 
 test('int validate numbers', () => {
   expect(validate(55)).toBeNull()
@@ -11,4 +11,20 @@ test('int validate numbers', () => {
 test('int validate non numbers', () => {
   expect(validate('abc')).toMatchObject({ error: ERROR.NOT_A_NUMBER, value: 'abc' })
   expect(validate('n700')).toMatchObject({ error: ERROR.NOT_A_NUMBER, value: 'n700' })
+})
+
+test('int cast', () => {
+  const number = IntParam()
+  expect(number.cast(555.44)).toEqual(555)
+  expect(number.cast(-3.44)).toEqual(-3)
+  expect(number.cast('15')).toEqual(15)
+  expect(number.cast('abc')).toEqual(0)
+  expect(number.cast()).toBeNull()
+})
+
+test('int cast required', () => {
+  const number = IntParam.options({ required: true })
+  expect(number.cast()).toEqual(0)
+  expect(number.cast(0)).toEqual(0)
+  expect(number.cast(144)).toEqual(144)
 })
