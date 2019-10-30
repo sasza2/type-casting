3f65d5a0a9031708a0e7d6e2b581946b7c9dba66
sasza <sasza@onet.eu>
2019-10-30 22:09:56 +0100

    string cast

--- src/params/StringParam.js
+++ src/params/StringParam.js
@@ -1,11 +1,18 @@
 import ERROR from '../errors'
 
-export const validate = (value) => {
+export const validate = (value, options = {}) => {
+  if (options.required && !value) return { error: ERROR.REQUIRED_BUT_EMPTY }
   if (!value) return { error: ERROR.IS_EMPTY }
   return null
 }
 
+export const cast = (value, options = {}) => {
+  if (!options.required && !value) return null
+  return value.toString()
+}
+
 const StringParam = (options) => ({
+  cast: (value) => cast(value, options),
   validate: (value) => validate(value, options),
 })
 
