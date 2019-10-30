378951dc9e8378713625f20a9ee11a1402b7a102
sasza <sasza@onet.eu>
2019-10-30 21:24:45 +0100

    int cast

--- src/params/IntParam.js
+++ src/params/IntParam.js
@@ -1,12 +1,20 @@
 import ERROR from '../errors'
+import { requiredButEmpty } from './helpers'
 
-export const validate = (value) => {
+export const validate = (value, options = {}) => {
+  if (requiredButEmpty(value, options)) return { error: ERROR.REQUIRED_BUT_EMPTY }
   const number = parseInt(value, 10)
   if (Number.isNaN(number)) return { value, error: ERROR.NOT_A_NUMBER }
   return null
 }
 
+export const cast = (value, options = {}) => {
+  if (!options.required && (value === null || value === undefined)) return null
+  return parseInt(value, 10) || 0
+}
+
 const IntParam = (options) => ({
+  cast: (value) => cast(value, options),
   validate: (value) => validate(value, options),
 })
 
